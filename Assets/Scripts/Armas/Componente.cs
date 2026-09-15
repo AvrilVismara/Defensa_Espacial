ce99d727af19f1a4a38d76123847fc0734c6e4bb
criisleoo-UTN <criisleoo@gmail.com>
2026-09-15 14:34:33 -0300

    cambios

--- Assets/Scripts/Armas/Componente.cs
+++ Assets/Scripts/Armas/Componente.cs
@@ -34,7 +34,7 @@ public class ComponenteArma : MonoBehaviour
         // Intentar crear el arma según el tipo de configuración
         if (configuracion is ConfigLanzaCohetesSO LanzaCohetesConfig)
         {
-            arma = LanzaCohetesaConfig.CrearArma();
+            arma = LanzaCohetesConfig.CrearArma();
         }
         else if (configuracion is ConfigAmetralladoraSO ametralladoraConfig)
         {
